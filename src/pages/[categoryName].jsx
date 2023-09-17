e8948c68f7f8ccdc0133728065bc55b11c48acae
Trinh Van Son <sontrinh.tp@gmail.com>
2023-09-17 17:32:00 +0700

    pagination category page done

--- src/pages/[categoryName].jsx
+++ src/pages/[categoryName].jsx
@@ -5,7 +5,7 @@ import Footer from '~/views/layouts/footer/Footer';
 import Header from '~/views/layouts/header/Header';
 import styles from '../styles/category.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faChevronDown, faAngleLeft, faAngleRight } from '@fortawesome/free-solid-svg-icons';
+import { faChevronDown, faAngleLeft, faAngleRight, faListUl, faTags, faArrowUpLong, faArrowDownLong } from '@fortawesome/free-solid-svg-icons';
 
 function CategoryPage({ category, products, subcategories, currentPage, totalPages }) {
     return (
@@ -14,53 +14,86 @@ function CategoryPage({ category, products, subcategories, currentPage, totalPag
             <div className="section">
                 <div className="container">
                     <div className="row">
-                        <div className="col-md-12">
-                            <h2 className={styles['heading']}>Sản phẩm cho {category ? category.name : ''}</h2>
-                        </div>
-                    </div>
-                    <div className="row">
                         <div className="col-md-3">
-                            <div className={styles['sidebar']}>
-                                <h3 className={styles['sub-heading']}>Phân loại</h3>
-                                <ul>
-                                    {subcategories && subcategories.map(subcategory => (
-                                        <li key={subcategory.id}>
-                                            <a href={`/subcategories/${subcategory.slug}`}>{subcategory.name}</a>
+                            <nav className={styles['sidebar']}>
+                                <div className={styles["categories"]}>
+                                    <h3 className={styles['categories-heading']}>
+                                        <FontAwesomeIcon className={styles["category-heading-icon"]} icon={faListUl} /> Danh mục
+                                    </h3>
+                                    <ul className={styles['category-list']}>
+                                        <li className={`${styles["category-item"]} ${styles["category-item--active"]}`}>
+                                            <a className={styles["category-item__link"]}>Cún cưng</a>
                                         </li>
-                                    ))}
-                                </ul>
-                            </div>
+                                        <li className={styles["category-item"]}>
+                                            <a className={styles["category-item__link"]}>Miu cưng</a>
+                                        </li>
+                                        <li className={styles["category-item"]}>
+                                            <a className={styles["category-item__link"]}>Nhà xinh</a>
+                                        </li>
+                                        <li className={styles["category-item"]}>
+                                            <a className={styles["category-item__link"]}>Làm đẹp</a>
+                                        </li>
+                                        <li className={styles["category-item"]}>
+                                            <a className={styles["category-item__link"]}>Tủ thuốc</a>
+                                        </li>
+                                        <li className={styles["category-item"]}>
+                                            <a className={styles["category-item__link"]}>Thời trang</a>
+                                        </li>
+                                        <li className={styles["category-item"]}>
+                                            <a className={styles["category-item__link"]}>Combo sản phẩm</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div className={styles["subcategories"]}>
+                                    <h3 className={styles['subcategories-heading']}>
+                                        <FontAwesomeIcon className={styles["category-heading-icon"]} icon={faTags} /> Loại sản phẩm
+                                    </h3>
+                                    <ul className={styles['category-list']}>
+                                        {subcategories && subcategories.map(subcategory => (
+                                            <li className={styles["category-item"]} key={subcategory.id}>
+                                                <a className={styles["category-item__link"]} href={`/subcategories/${subcategory.slug}`}>{subcategory.name}</a>
+                                            </li>
+                                        ))}
+                                    </ul>
+                                </div>
+                            </nav>
                         </div>
                         <div className="col-md-9">
                             <div className="row">
-                                <div className={styles.filter}>
+                                <div className={styles["filter"]}>
                                     <span className={styles["filter__title"]}>Sắp xếp theo</span>
-                                    <button className={`${styles["filter__btn"]} ${styles.btn}`}>Phổ biến</button>
-                                    <button className={`${styles["filter__btn"]} ${styles.btn} ${styles.active}`}>Mới nhất</button>
-                                    <button className={`${styles["filter__btn"]} ${styles.btn}`}>Bán chạy</button>
+                                    <button className={`${styles["filter__btn"]} ${styles["btn"]}`}>Khuyến mãi</button>
+                                    <button className={`${styles["filter__btn"]} ${styles["btn"]} ${styles["active"]}`}>Mới nhất</button>
+                                    <button className={`${styles["filter__btn"]} ${styles["btn"]}`}>Bán chạy</button>
                                     <div className={styles["select-input"]}>
-                                        <span className={styles["select-input__lable"]}>giá</span>
+                                        <span className={styles["select-input__lable"]}>Giá</span>
                                         <FontAwesomeIcon className={styles["select-input__icon"]} icon={faChevronDown} />
                                         <ul className={styles["select-input__list"]}>
                                             <li className={styles["select-input__item"]}>
-                                                <a href="" className={styles["select-input__link"]}>Giá: Thấp đến cao</a>
+                                                <a href="" className={styles["select-input__link"]}>Giá: Thấp đến cao <FontAwesomeIcon className={styles['select-input__link-icon']} icon={faArrowUpLong} /></a>
                                             </li>
                                             <li className={styles["select-input__item"]}>
-                                                <a href="" className={styles["select-input__link"]}>Giá: Cao đến thấp</a>
+                                                <a href="" className={styles["select-input__link"]}>Giá: Cao đến thấp <FontAwesomeIcon className={styles['select-input__link-icon']} icon={faArrowDownLong} /></a>
                                             </li>
                                         </ul>
                                     </div>
 
                                     <div className={styles["filter__page"]}>
                                         <span className={styles["filter__page-num"]}>
-                                            <span className={styles["filter__page-current"]}>1</span>/4
+                                            Trang: <span className={styles["filter__page-current"]}>{currentPage} </span>/ {totalPages}
                                         </span>
 
                                         <div className={styles["filter__page-control"]}>
-                                            <a href="" className={`${styles["filter__page-btn"]} ${styles["filter__page-btn-disable"]}`}>
+                                            <a
+                                                href={`/${category.slug}?page=${Math.max(currentPage - 1, 1)}`}
+                                                className={`${styles["filter__page-btn"]} ${currentPage === 1 ? styles["filter__page-btn-disable"] : ""}`}
+                                            >
                                                 <FontAwesomeIcon icon={faAngleLeft} />
                                             </a>
-                                            <a href="" className={styles["filter__page-btn"]}>
+                                            <a
+                                                href={`/${category.slug}?page=${Math.min(currentPage + 1, totalPages)}`}
+                                                className={`${styles["filter__page-btn"]} ${currentPage === totalPages ? styles["filter__page-btn-disable"] : ""}`}
+                                            >
                                                 <FontAwesomeIcon icon={faAngleRight} />
                                             </a>
                                         </div>
@@ -76,19 +109,55 @@ function CategoryPage({ category, products, subcategories, currentPage, totalPag
                                     ))}
                                 </div>
                             </div>
-                        </div>
-                    </div>
-                    <div className="row">
-                        {Array.from({ length: totalPages }, (_, index) => (
-                            <a key={index} href={`/${category.slug}?page=${index + 1}`}>
-                                {index + 1}
-                            </a>
-                        ))}
-                    </div>
-                </div>
-            </div>
+                            <div className="row">
+                                <Pagination totalPages={totalPages} category={category} currentPage={currentPage} />
+                            </ div >
+                        </div >
+                    </ div >
+                </div >
+            </ div >
             <Footer />
         </>
+    );
+}
+
+export function Pagination({ totalPages, category, currentPage }) {
+    return (
+        <div className="row">
+            <ul className={styles["pagination"]}>
+                <li className={styles["pagination-item"]}>
+                    <a
+                        href={`/${category.slug}?page=${Math.max(currentPage - 1, 1)}`}
+                        className={`${styles["filter__page-btn"]} ${currentPage === 1 ? styles["filter__page-btn-disable"] : ""}`}
+                    >
+                        <FontAwesomeIcon icon={faAngleLeft} />
+                    </a>
+                </li>
+
+                {Array.from({ length: totalPages }).map((_, index) => (
+                    <li
+                        key={index}
+                        className={`${styles["pagination-item"]} ${currentPage === index + 1 ? styles["pagination-item--active"] : ""}`}
+                    >
+                        <a
+                            href={`/${category.slug}?page=${index + 1}`}
+                            className={styles["pagination-item__link"]}
+                        >
+                            {index + 1}
+                        </a>
+                    </li>
+                ))}
+
+                <li className={styles["pagination-item"]}>
+                    <a
+                        href={`/${category.slug}?page=${Math.min(currentPage + 1, totalPages)}`}
+                        className={`${styles["filter__page-btn"]} ${currentPage === totalPages ? styles["filter__page-btn-disable"] : ""}`}
+                    >
+                        <FontAwesomeIcon icon={faAngleRight} />
+                    </a>
+                </li>
+            </ul>
+        </div>
     );
 }
 
